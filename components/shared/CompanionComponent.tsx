3be96ab07d7d7e1bdbef438650fce87fbca8c25f
Hamed <ostovari.eng@gmail.com>
2025-06-05 22:18:35 +0200

    Implemented Voice AI functionality

--- components/shared/CompanionComponent.tsx
+++ components/shared/CompanionComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn, getSubjectColor } from "@/lib/utils";
+import { cn, configureAssistant, getSubjectColor } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import Error from "next/error";
@@ -27,7 +27,8 @@ const CompanionComponent: FC<CompanionComponentProps> = ({
   const lottieRef = useRef<LottieRefCurrentProps>(null);
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [isSpeaking, setIsSpeaking] = useState(false);
-  const [isMuted, setIsMuted] = useState(true);
+  const [isMuted, setIsMuted] = useState(false);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
   useEffect(() => {
     if (lottieRef) {
       if (isSpeaking) lottieRef.current?.play();
@@ -38,7 +39,13 @@ const CompanionComponent: FC<CompanionComponentProps> = ({
   useEffect(() => {
     const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
     const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
-    const onMessage = () => {};
+
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [newMessage, ...prev]);
+      }
+    };
     const onError = (error: Error) => console.log(error);
     const onSpeechStart = () => setIsSpeaking(true);
     const onSpeechEnd = () => setIsSpeaking(false);
@@ -65,9 +72,23 @@ const CompanionComponent: FC<CompanionComponentProps> = ({
     setIsMuted((prev) => !prev);
   };
 
-  const handleCall = async () => {};
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
 
-  const handleDisconnect = async () => {};
+    const assistantOverrides = {
+      variableValues: { subject, topic, style },
+      clientMessages: ["transcript"],
+      serverMessages: [],
+    };
+
+    // @ts-expect-error
+    vapi.start(configureAssistant(voice, style), assistantOverrides);
+  };
+
+  const handleDisconnect = async () => {
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  };
 
   return (
     <section className="flex flex-col h-[70vh] ">
@@ -126,9 +147,13 @@ const CompanionComponent: FC<CompanionComponentProps> = ({
             )}
             <p className="font-bold text-2xl">{userName ?? "User"}</p>
           </div>
-          <button className="btn-mic" onClick={toggleMicrophone}>
+          <button
+            className="btn-mic"
+            onClick={toggleMicrophone}
+            disabled={callStatus !== CallStatus.ACTIVE}
+          >
             {isMuted ? <MicOff /> : <Mic />}
-            <p className="max-sm:hidden">Turn {isMuted ? "off" : "on"} mic</p>
+            <p className="max-sm:hidden">Turn {isMuted ? "on" : "off"} mic</p>
           </button>
           <button
             className={cn(
@@ -150,10 +175,22 @@ const CompanionComponent: FC<CompanionComponentProps> = ({
       </div>
       <section className="transcript">
         <div className="transcript-message no-scrollbar">
-          <p>Message</p>
-          <p>Message</p>
-          <p>Message</p>
-          <p>Message</p>
+          {messages.map((message, index) => {
+            if (message.role === "assistant") {
+              return (
+                <p key={index} className="max-sm:text-sm">
+                  {name.split(" ")[0].replace("/[.,]/g, ", "")}:{" "}
+                  {message.content}
+                </p>
+              );
+            } else {
+              return (
+                <p key={index} className="text-primary max-sm:text-sm">
+                  {userName}: {message.content}
+                </p>
+              );
+            }
+          })}
         </div>
         <div className="transcript-fade" />
       </section>
